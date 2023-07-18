314be7aa2bf6190c6a6a800e272c9ea14966591f
dzakyadlh <dzakyadlh@gmail.com>
2023-07-18 22:01:30 +0700

    updates

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,9 @@ import Search from "../../components/search";
 import CurrentWeather from "../../components/currentWeather";
 import Forecast from "../../components/forecast";
 import { WEATHER_API_KEY, weatherAPIURL } from "../../api";
+import Sunny from "../../assets/icons/sun.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWind, faDroplet } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
 const Home = () => {
@@ -37,6 +40,54 @@ const Home = () => {
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
+      <div className="current-weather">
+        <img
+          src={Sunny}
+          alt="current-weather-icon"
+          className="current-weather-icon"
+        />
+        <h1 className="current-temp">
+          26<span>&deg;C</span>
+        </h1>
+        <h2 className="current-cond">Sunny</h2>
+        <div className="current-params">
+          <div className="params">
+            <FontAwesomeIcon icon={faWind} className="current-params-icon" />
+            <p>
+              2<span>m/s</span>
+            </p>
+          </div>
+          <div className="params">
+            <FontAwesomeIcon icon={faDroplet} className="current-params-icon" />
+            <p>
+              45<span>%</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="daily-forecast">
+        <div className="forecast-card">
+          <img src={Sunny} alt="forecast-icon" className="forecast-icon" />
+          <h3 className="forecast-temp">
+            27<span>&deg;C</span>
+          </h3>
+          <h4 className="forecast-day">Thursday</h4>
+        </div>
+        <div className="forecast-card">
+          <img src={Sunny} alt="forecast-icon" className="forecast-icon" />
+          <h3 className="forecast-temp">
+            27<span>&deg;C</span>
+          </h3>
+          <h4 className="forecast-day">Thursday</h4>
+        </div>
+        <div className="forecast-card">
+          <img src={Sunny} alt="forecast-icon" className="forecast-icon" />
+          <h3 className="forecast-temp">
+            27<span>&deg;C</span>
+          </h3>
+          <h4 className="forecast-day">Thursday</h4>
+        </div>
+      </div>
     </div>
   );
 };
